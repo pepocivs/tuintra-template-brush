a6dc5116915bfa0461d250664376a60da0867dfd
PepoCivs <tcbpepo@gmail.com>
2019-06-12 21:36:50 +0200

    style: using grid instead absolute position

--- src/components/footer.js
+++ src/components/footer.js
@@ -13,13 +13,9 @@ export default class Footer extends React.Component {
 		};
 		return (
 			<div>
-				<div className="footer footer-separator">
-					<FooterSeparator color={this.clubInfo.options.secundary_color_web} />
-				</div>
-				<div className="footer">
-					<div style={footerColor} className="footer-content">
-						Footer stuff
-					</div>
+				<FooterSeparator color={this.clubInfo.options.secundary_color_web} />
+				<div style={footerColor} className="footer-content">
+					Footer stuff
 				</div>
 			</div>
 		)
